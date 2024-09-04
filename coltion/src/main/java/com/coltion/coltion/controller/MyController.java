027f20ea4387436eda5ea3fea3cf3245091acb64
TerRiek <ycjeong99@gmail.com>
2024-09-04 11:13:43 +0900

    스프링부트 리액트 연동 초기 설정

---
+++ coltion/src/main/java/com/coltion/coltion/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.coltion.coltion.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/api/test")
+	public String hello() {
+		return "Hello World";
+	}
+}
